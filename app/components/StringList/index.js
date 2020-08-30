a12b83e735d3b04ca202219dfd1013ede39f6aae
Angela Lee <rubii.kun@gmail.com>
2020-08-29 18:14:58 -0700

    Implement StringList UI

---
+++ app/components/StringList/index.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * StringList
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function StringList({ strings }) {
+  const list = strings.map(string => <div>{string}</div>);
+
+  return <div>{list}</div>;
+}
+
+StringList.propTypes = {
+  strings: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default StringList;
