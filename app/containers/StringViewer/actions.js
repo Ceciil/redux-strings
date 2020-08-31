7bf57e3c68dcefe6f6e5b8b16c3e3e1a1c6598d8
Angela Lee <rubii.kun@gmail.com>
2020-08-31 16:03:22 -0700

    Add constants and actions to StringViewer

--- app/containers/StringViewer/actions.js
+++ app/containers/StringViewer/actions.js
@@ -4,10 +4,32 @@
  *
  */
 
-import { DEFAULT_ACTION } from './constants';
+import {
+  DEFAULT_ACTION,
+  UPDATE_STRINGS_REQUEST,
+  UPDATE_STRINGS_SUCCESS,
+  UPDATE_STRINGS_FAILURE,
+} from './constants';
 
 export function defaultAction() {
   return {
     type: DEFAULT_ACTION,
+  };
+}
+
+export function updateString() {
+  return {
+    type: UPDATE_STRINGS_REQUEST,
+  };
+}
+export function stringUpdated(strings) {
+  return {
+    type: UPDATE_STRINGS_SUCCESS,
+    strings,
+  };
+}
+export function stringUpdatingError() {
+  return {
+    type: UPDATE_STRINGS_FAILURE,
   };
 }
--- app/containers/StringViewer/constants.js
+++ app/containers/StringViewer/constants.js
@@ -4,4 +4,10 @@
  *
  */
 
-export const DEFAULT_ACTION = 'app/StringViewer/DEFAULT_ACTION';
+export const DEFAULT_ACTION = 'redux-strings/App/StringViewer/DEFAULT_ACTION';
+export const UPDATE_STRINGS_REQUEST =
+  'redux-strings/App/StringViewer/UPDATE_STRINGS_REQUEST';
+export const UPDATE_STRINGS_SUCCESS =
+  'redux-strings/App/StringViewer/UPDATE_STRINGS_SUCCESS';
+export const UPDATE_STRINGS_FAILURE =
+  'redux-strings/App/StringViewer/UPDATE_STRINGS_FAILURE';
